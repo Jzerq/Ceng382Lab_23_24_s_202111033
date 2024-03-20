8cd7d912a871b074f38396872f4cd070d5feaa7c
Jzerq <dogukangundem@hotmail.com>
2024-03-20 11:55:02 +0300

    lab 5 created new page added

---
+++ dotnetLab/Pages/Ceng382Page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class Ceng382PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- dotnetLab/obj/Debug/net8.0/dotnetLab.AssemblyInfo.cs
+++ dotnetLab/obj/Debug/net8.0/dotnetLab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c5d7d1f44465d944171582c3a03acad106bd1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1eedf9cacbb51db326546a90acfa35b6c2c6bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
