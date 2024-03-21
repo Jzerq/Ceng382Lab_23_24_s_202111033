7b7d5375f6e755f47f4b21e52799111d51e72301
Jzerq <dogukangundem@hotmail.com>
2024-03-21 23:17:39 +0300

    toggle and calculator added

--- dotnetLab/wwwroot/js/lab5script.js
+++ dotnetLab/wwwroot/js/lab5script.js
@@ -1,7 +1,33 @@
 function hidelements(){
-    alert("Elements Hidden/");
-    let hide=document.getElementById("PRicce");
-    //hide.style.display="none";
-    hide.innerHTML="Elements Hidden";
+    let elt=document.getElementById("PRicce");
+    var newText=document.createTextNode("toggling");
+
+    if(elt.style.display=="none"){
+        elt.style.display="block";
+    }
+    else{
+        elt.style.display="none";
+    }
    
 }
+function hidecal(){
+   
+   
+}
+
+
+function calculateSum() {
+
+    const num1=Number(document.getElementById("num1").value);
+    const num2=Number(document.getElementById("num2").value);
+    const result=document.getElementById("result");
+    let sum=num1+num2;
+
+    result.innerHTML=`The sum of ${num1} and ${num2} is ${sum}`;
+}
+
+
+
+
+
+
