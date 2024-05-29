d5d68517770c02fec3ab626e477bf8b723a49287
Jzerq <dogukangundem@hotmail.com>
2024-05-29 22:47:07 +0300

    lab project final

--- WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
+++ WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19daad2a6cd4f4cc3216200e4dc954ff71fdbeb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39dd6c1c5dee2bd699c08fed2eb26d6869940644")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
